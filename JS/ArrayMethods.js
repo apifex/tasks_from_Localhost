5fe8f087663f2caaf0044fbf238cc798c08a7ebe
Pawel Przebieda <pprzeb@gmail.com>
2020-10-18 20:30:08 +0200

    array methods - done

--- JS/ArrayMethods.js
+++ JS/ArrayMethods.js
@@ -11,17 +11,93 @@
 
 // skopiuj identyczne działanie tych metod za pomocą pętli for lub while
 // w funkcjach
+let testArray = [2, 4, 'fd', 'lala']
 
-const forEachFn = (array, callback) => {}
 
-const mapFn = (array, callback) => {}
+const forEachFn = (array, callback) => {
+    for(let i=0; i<array.length; i++) {
+        callback(array[i], i, array)
+    }
+}
 
-const entriesFn = (array, callback) => {}
+const mapFn = (array, callback) => {
+    let mapedArray = [];
+    for(let i=0; i<array.length; i++) {
+        mapedArray.push(callback(array[i], i, array))
+    }
+    return mapedArray
+}
 
-const filterFn = (array, callback) => {}
+const entriesFn = (array) => {
+    let output = {};
+    for (let i=0; i<array.length; i++) {
+        output[i] = [i, array[i]]
+    }
 
-const reduceFn = (array, callback, inital) => {}
+    output[Symbol.iterator] = function(){
+        return {
+            current: 0,
+            last: array.length-1,
 
-const everyFn = (array, callback) => {}
+            next() {
+                if (this.current <= this.last) {
+                    return {done: false, value: output[this.current++]}
+                } else {
+                    return {done: true}
+                }
+            }
+        }
+    }
+    let current = 0;
+    let last = array.length;
+    output.next = function(){
+        if (current <= last) {
+            return {done: false, value: output[current++]}
+        } else {
+            return {done: true}
+        }
+    }
+        
+    
+    return output
+}
 
-const someFn = (array, callback) => {}
+const filterFn = (array, callback) => {
+    let filteredArray = []
+    for (let i=0; i<array.length; i++) {
+        if (callback(array[i], i, array)) {
+            filteredArray.push(array[i])
+        } 
+    }
+    return filteredArray
+}
+
+const reduceFn = (array, callback, initial) => {
+        for (let i=0; i<array.length; i++) {
+            if (initial) {
+                initial = callback(initial,array[i], i-1, array);
+            } else { 
+                initial = callback(array[0], array[i+1], i, array);
+                i++;
+            }
+            
+    } 
+    return initial    
+}
+
+const everyFn = (array, callback) => {
+    for (let i=0; i<array.length; i++) {
+        let result = callback(array[i], i, array);
+        if (!result) return false
+        console.log(i)
+    }
+    return true
+}
+
+const someFn = (array, callback) => {
+    for (let i=0; i<array.length; i++) {
+        let result = callback(array[i], i, array);
+        if (result) return true     
+    }
+    return false
+}
