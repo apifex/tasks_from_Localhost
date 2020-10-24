9dc6a6e277bdbbba4a773dbd45cbc9a74bc55fb3
Pawel Przebieda <apifex@gmail.com>
2020-10-24 21:29:22 +0200

    User.js -DONE

--- JS/User.js
+++ JS/User.js
@@ -1,7 +1,4 @@
 // Stwórz klasę dla struktury danych związanych z użytkowniem
-class User{
-
-}
 // Klasa przyjmować w konstruktorze: 
 // Imię, Nazwisko, datę urodzenia, hasło, 
 // płeć, adres email, poziom dostepu = "user"
@@ -19,16 +16,126 @@ class User{
 // - data (nieważne jaka wejdzie) do konstruktora musi wejść w formacie MM/DD/YYYY				
 // - imię i nazwisko musi być niepuste				
 
+const is = require('is_js');
+const moment = require('moment');
+
+    class User{
+        constructor(name, surname, dateOfBirth, password, sex, mail, accessLevel = 'user') {
+            this.name = name;
+            this.surname = surname;
+            this.dateOfBirth = dateOfBirth;
+            this.password = password;
+            this.sex = sex;
+            this.mail = mail;
+            this.accessLevel = accessLevel
+        }
+
+        changeMail (newMail) {
+            this.mail = newMail
+        }
+
+        get name(){
+            return this._name
+        }
+
+        set name(value) {
+            if (is.empty(value)) {
+                throw console.error("Name shoud not be empty");
+            }
+            this._name = value
+        }
+        
+        get surname() {
+            return this._surname
+        }
+
+        set surname(value) {
+            if (is.empty(value)) {
+                throw console.error("Surname shoud not be empty")
+            }
+            this._surname = value
+        }
+
+        get dateOfBirth() {
+            return this._dateOfBirth
+        }
+
+        set dateOfBirth(value) {
+            this._dateOfBirth = moment(value).format('MM/DD/YYYY');
+        }
+
+        get password() {
+            return this._password
+        }
+
+        set password(value) {
+            let regExp = /^(?=.*\d)(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/
+            if (!regExp.test(value)) {
+                throw console.error("Password shoud have at least 8 characters with 1 Capital, 1 number, and 1 special char")
+            }
+            this._password = value
+        }
+
+        get sex() {
+            return this._sex
+        }
+
+        set sex(value) {
+            if (value!=='male' && value!=='female') {
+                throw console.error("Sex shoud be MALE or FEMALE")
+            }
+            this._sex = value
+        }
+
+        get mail() {
+            return this._mail
+        }
+
+        set mail(value) {
+            if (!is.email(value)) {
+                throw console.error("Email is incorrect")
+            }
+            this._mail = value
+        }
+
+    }
+
 
 // Stwórz klasę dla struktury danych związanych z administratorem
 // Klasa ma dziedziczyć po klasie User wszystkie informacje i metody
-class Administrator{
-
-}
-
 // Klasa ma mieć poziom dostępu = "admin"
 // Klasa ma umożliwiać: 
 // - zmianę poziomu dostępu dla innego Usera
 // - zmianę hasła dla innego Usera
+
+class Administrator extends User{
+    constructor(name, surname, dateOfBirth, password, sex, mail, accessLevel = 'admin') {
+        super(name, surname, dateOfBirth, password, sex, mail);
+        this.accessLevel = accessLevel
+    }
+    
+    changeAccessLevel(user, accessLevel) {
+        user.accessLevel = accessLevel;
+        if (accessLevel === 'admin') {
+            user.changePassword = this.changePassword;
+            user.changeAccessLevel = this.changeAccessLevel
+        };
+
+        if (accessLevel === 'user') {
+            delete user.changePassword;
+            delete user.changeAccessLevel;
+        }
+        
+    }
+
+    changePassword(user, newPassword) {
+        user.password = newPassword
+    }
+
+    
+}
+
+
+
 
 		
